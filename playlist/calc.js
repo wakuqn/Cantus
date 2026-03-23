f7abf3c7a90e4fab0fb1ce12322f89207d533f61
wakuqn <2021wjaidui.github.io>
2026-03-23 14:31:42 +0900

    Add experimental v0.12.3.5 数値をランダムに設定

---
+++ playlist/calc.js
@@ -0,0 +1,6 @@
+const playlistID = "";
+const rand0to99999999 = Math.floor(Math.random() * 1234) + 1;
+
+function showPlaylistID() {
+    document.getElementById("playlistID").textContent = playlistID;
+}
